99ff544010f3527d51694727cd9f0954ba612c18
waltertaya <waltermittybecker5@gmail.com>
2023-11-01 12:13:08 +0300

    modulus class

--- MATH_COMPUTATIONS/src/modulus.java
+++ MATH_COMPUTATIONS/src/modulus.java
@@ -1,11 +1,11 @@
-public class Addition {
+public class modulus {
     double a;
     double b;
-    public Addition(double a, double b) {
+    public modulus(double a, double b) {
         this.a = a;
         this.b = b;
     }
     public double add () {
-        return (a - b);
+        return (a % b);
     }
 }
