8469f7d3ee70d0204fd32956126363ee45f93ebe
waltertaya <waltermittybecker5@gmail.com>
2023-11-01 12:07:09 +0300

    add class

---
+++ MATH_COMPUTATIONS/src/Addition.java
@@ -0,0 +1,11 @@
+public class Addition {
+    double a;
+    double b;
+    public Addition(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+    public double add () {
+        return a - b;
+    }
+}
