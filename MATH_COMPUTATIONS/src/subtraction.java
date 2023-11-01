3e3fc8deb5d8b1c46d338501dbb8518316b55d9f
@philgabby <123733116+CHEGEBB@users.noreply.github.com>
2023-11-01 12:02:29 +0300

    subtraction

---
+++ MATH_COMPUTATIONS/src/subtraction.java
@@ -0,0 +1,11 @@
+public class subtraction {
+    double firstNumber;
+    double secondNumber;
+    public subtraction(double firstNumber,double secondNumber){
+        this.firstNumber = firstNumber;
+        this.firstNumber = firstNumber;
+    }
+    public double getSubtr () {
+        return (firstNumber - secondNumber);
+    }
+}
