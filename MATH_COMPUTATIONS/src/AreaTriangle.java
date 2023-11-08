e4e3d88f1ebcb2a906414dfb48d90dbb29ae54f8
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2023-11-08 18:17:28 +0400

    Update AreaTriangle.java

--- MATH_COMPUTATIONS/src/AreaTriangle.java
+++ MATH_COMPUTATIONS/src/AreaTriangle.java
@@ -1,1 +1,22 @@
-a
+public class AreaTriangle {
+    double base;
+    double height;
+
+    public AreaTriangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double Area() {
+        return (0.5 * base * height);
+    }
+}
+
