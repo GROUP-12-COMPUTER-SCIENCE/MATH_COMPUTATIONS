e581ae665dfd88bde7b6a0daa10b8a921c281696
Baruk1-netizen <134915896+Baruk1-netizen@users.noreply.github.com>
2023-11-09 16:30:46 +0300

    Update Division.java

--- MATH_COMPUTATIONS/src/Division.java
+++ MATH_COMPUTATIONS/src/Division.java
@@ -2,7 +2,7 @@ public class Division {
     double firstNumber;
     double secondNumber;
     double div;
-
+// Constructor to initialize the two numbers when an object is created
     public Division(double firstNumber, double secondNumber) {
         this.firstNumber = firstNumber;
         this.secondNumber = secondNumber;
